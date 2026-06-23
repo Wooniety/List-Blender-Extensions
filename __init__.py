b7270a771ac25b2e5720abb7669b209f74cd7c33
Heng Woon <wooniety@gmail.com>
2026-06-23 18:19:15 +0800

    setup

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from list_extensions import ListBlenderExtensions
+import bpy
+
+def register():
+    bpy.utils.register_class(ListBlenderExtensions)
---
+++ list_extensions.py
@@ -0,0 +1,37 @@
+import bpy
+import addon_utils
+
+class ListBlenderExtensions:
+    def __init__(self):
+        self.addons = []
+        self.filepath = self.setFileDirectory()
+        self.addonsFile = ""
+
+        self.toggleAuthor = False
+        self.toggleVersionNum = True
+        self.toggleCategory = False
+        self.toggleShowOnlyEnabled = True
+    
+    def setFileDirectory(self, filepath:str):
+        # if empty set default to be somewhere
+        pass
+
+    def loadListOfAddons(self):
+        self.addons = []
+        for addon in addon_utils.modules():
+            self.addons.append(addon_utils.module_bl_info(addon))
+        
+    def createAddonList(self):
+        pass
+
+    def printAddonList(self):
+        print(self.addons)
+    
+    def exportAddonListToCSV(self):
+        pass
+    
+    def exportAddonListToTXT(self):
+        pass
+    
+    def saveFile(self):
+        pass
