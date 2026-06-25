f7aac67782f080bc431427932463b2db90d0cf38
Heng Woon <wooniety@gmail.com>
2026-06-25 17:06:38 +0800

    checkpoint

--- __init__.py
+++ __init__.py
@@ -1,29 +1,43 @@
 import bpy
 # from list_extensions.py import ListBlenderExtensions
 
-class LISTEXTENSIONS_OT_hello(bpy.types.Operator):
-    bl_idname = "list_extensions.say_hello"
-    bl_label = "Say Hello"
-    bl_description = "Prints a message to the console"
+class LISTEXTENSIONS_(bpy.types.Operator):
+    bl_idname = "list_extensions.export_extensions"
+    bl_label = "Export"
 
     def execute(self, context):
-        self.report({'INFO'}, "Hello from Blender Addon!")
-        print("Hello from Blender Addon!")
+        return {'FINISHED'}
+
+class LISTEXTENSIONS_OT_exportExtensions(bpy.types.Operator):
+    bl_idname = "list_extensions.export_extensions"
+    bl_label = "Export"
+
+    def execute(self, context):
         return {'FINISHED'}
 
 class LISTEXTENSIONS_PT_Panel(bpy.types.Panel):
     bl_label = 'List Extensions'
-    bl_id = 'LISTEXTENSIONS_PT_Panel'
+    bl_idname = 'LISTEXTENSIONS_PT_Panel'
     bl_category = "Tool"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
 
     def draw(self, context):
         layout = self.layout
-        layout.operator("LISTEXTENSIONS.say_hello")
+        layout.label(text="Show Additional Info")
+        layout.separator()
+        layout.prop(context.object, "list_extensions.toggle_author", text="Author")
+        layout.prop(context.object, "list_extensions.toggle_vernum", text="Version No.")
+        layout.prop(context.object, "list_extensions.toggle_category", text="Category")
+        layout.prop(context.object, "list_extensions.toggle_website", text="Website")
+        layout.label(text="Export As")
+        layout.seperator(context.object, "Filter Only Enabled Extensions")
+        layout.operator("list_extensions.export_extensions")
 
 def register():
-    bpy.utils.register_class(LISTEXTENSIONS_OT_hello)
+    bpy.utils.register_class(LISTEXTENSIONS_OT_exportExtensions)
+    bpy.utils.register_class(LISTEXTENSIONS_PT_Panel)
 
 def unregister():
-    bpy.utils.unregister_class(LISTEXTENSIONS_OT_hello)
+    bpy.utils.unregister_class(LISTEXTENSIONS_PT_Panel)
+    bpy.utils.unregister_class(LISTEXTENSIONS_OT_exportExtensions)
